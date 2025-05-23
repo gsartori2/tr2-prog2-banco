6547a5e376c78b15a8e2a82d931cc12d9cf41b83
gsartori2 <ghs327363@gmail.com>
2025-05-22 22:49:44 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private static final String Banco = "jdbc:postgresql://localhost:1227/postgres";
+    private static final String User = "postgres";
+    private static final String Senha = "27022006Gs";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection conexao = DriverManager.getConnection(Banco, User, Senha)) {
+            System.out.println("sucesso ao conectar");
+        } catch (SQLException e) {
+            System.out.println("erro ao conectar no banco de dados: " + e.getMessage());
+        }
+        ;
     }
 }
